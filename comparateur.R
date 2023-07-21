e7f8cdc2ba79a3dec0971ae09f167ed3d80852e8
louis-ddc <95404225+louis-ddc@users.noreply.github.com>
2023-07-21 23:01:55 +0200

    Update comparateur.R
    seuil et sélection sur tableau X

--- comparateur.R
+++ comparateur.R
@@ -1,11 +1,9 @@
 # Charger les bibliothèques nécessaires
 library(dplyr)
-library(tidyr)
 
 # Créer une fonction pour compter les occurrences des termes dans un tableau
 compter_termes <- function(tableau) {
   tableau %>%
-    unlist() %>%
     table() %>%
     as.data.frame() %>%
     arrange(desc(Freq)) %>%
@@ -20,19 +18,26 @@ creer_tableau_occurrences <- function(liste_tableaux) {
   # Compter les occurrences de chaque terme
   comptage <- compter_termes(tous_les_termes)
   
-  return(comptage)
+  # Filtrer les lignes avec une fréquence supérieure ou égale au nombre de tableaux moins deux
+  nb_tableaux <- length(liste_tableaux)
+  seuil <- nb_tableaux - 2
+  tableau_final <- comptage %>%
+    filter(Freq >= seuil)
+  
+  return(tableau_final)
 }
 
-# Charger les tableaux à partir des fichiers CSV
-tableau1 <- read.csv("deck1.csv", header = FALSE)$V1
-tableau2 <- read.csv("deck2.csv", header = FALSE)$V1
-tableau3 <- read.csv("deck3.csv", header = FALSE)$V1
+# Charger les tableaux à partir des fichiers texte (.txt)
+tableau1 <- readLines("deck1.txt")
+tableau2 <- readLines("deck2.txt")
+tableau3 <- readLines("deck3.txt")
 
 # Créer le tableau final des occurrences
 tableau_final <- creer_tableau_occurrences(list(tableau1, tableau2, tableau3))
 
-# Sauvegarder le résultat au format CSV
-write.csv(tableau_final, file = "tableau_occurrences.csv", row.names = FALSE)
+# Filtrer le nouveau tableau avec les fréquences supérieures à 2 et les termes présents dans le tableau1
+tableau_final_filtre <- tableau_final %>%
+  filter(Freq > 2 & Termes %in% tableau1)
 
-# Afficher le tableau final dans la console
-print(tableau_final)
+# Afficher le nouveau tableau filtré dans la console
+print(tableau_final_filtre)
